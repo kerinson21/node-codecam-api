859e0c10433cf7fcabdbe2d9de67df3a5d1b08dd
Kerinson Garcia <kerinson21garcia@gmail.com>
2024-08-23 16:41:46 -0600

    Se agrega el metodo create que inserta los datos a la base de datos

--- services/productos.service.js
+++ services/productos.service.js
@@ -1,4 +1,6 @@
+const { Sequelize } = require('sequelize');
 const sequelize = require('./../db/mssql');
+const boom = require('@hapi/boom');
 
 
 class PruductoService{
@@ -13,6 +15,28 @@ class PruductoService{
       metadata
     }
   }
-
+  async create(body){
+    const query = 'EXEC sp_insertarProducto :categoriaProducto_idCategoriaProducto, '+
+                  + ':usuario_idUsuario, :nombre, :marca, :codigo, :stock, :estado_idEstado, :precio, :foto';
+    try{
+      await sequelize.query(query, {
+        replacements: {
+          categoriaProducto_idCategoriaProducto: body.categoriaProducto_idCategoriaProducto,
+          usuario_idUsuario: body.usuario_idUsuario,
+          nombre: body.nombre,
+          marca: body.marca,
+          codigo: body.codigo,
+          stock: body.stock,
+          estado_idEstado: body.estado_idEstado,
+          precio: body.precio,
+          foto: body.foto
+        },
+        type: Sequelize.QueryType.INSERT
+      });
+      return ['El registro fue creado correctamente', body];
+    }catch(error){
+       throw boom.preconditionFailed('fallo el guardado de los datos');
+    }
+  }
 }
 
