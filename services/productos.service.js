91b6777c8698c39c956b369ee0e1f3500058515b
Kerinson Garcia <kerinson21garcia@gmail.com>
2024-08-23 16:20:19 -0600

    Se crea el objeto ProductoService y se crea el metodo find, este metodo devuelve todos los campos de la tabla productos

--- services/productos.service.js
+++ services/productos.service.js
@@ -1,8 +1,17 @@
-const siquelize = require('./../db/mssql');
+const sequelize = require('./../db/mssql');
+
 
 class PruductoService{
   constructor(){
 
+  }
+  async find(){
+    const query = 'SELECT * FROM productos';
+    const [data, metadata] = await sequelize.query(query);
+    return {
+      data,
+      metadata
+    }
   }
 
 }
