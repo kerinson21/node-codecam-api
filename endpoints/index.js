e45ef56b5c69bb5a793bb4e7665ed42a2de41298
Kerinson Garcia <kerinson21garcia@gmail.com>
2024-08-23 17:56:26 -0600

    Se agregan las rutas que van hacer utilizandas para los datos de los produtos

--- endpoints/index.js
+++ endpoints/index.js
@@ -3,12 +3,14 @@ const express = require('express');
 
 const estadosRouter = require('./estado.routes');
 const categoriaRouter = require('./categoria.routes');
+const productoRouter = require('./prodcuto.routes');
 
 function routerApi(app){
   const router = express.Router();
   app.use('/api/v1', router);
   router.use('/estados', estadosRouter);
   router.use('/categoria', categoriaRouter);
+  router.use('/productos', productoRouter);
 }
 
 module.exports = routerApi;
