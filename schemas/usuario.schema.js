fef9e80ebfea5be37bdb8172d311a451ac354bc4
Kerinson Garcia <kerinson21garcia@gmail.com>
2024-08-24 12:09:41 -0600

    Se agregan los esquemas y se empiezan a crear los servicios, se renonmbra el archivo de la conexion a la DB

---
+++ schemas/usuario.schema.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+
+const idUsuario = Joi.number().integer();
+const rol_rol = Joi.number().integer();
+const estado_idEstado = Joi.number().integer();
+const correo_electronico = Joi.string();
+const nombre_completo = Joi.string().min(5);
+const password = Joi.string().min(8);
+const telefono = Joi.string();
+const fecha_nacimiento= Joi.date();
+
+const createUsuarioSchema = Joi.object({
+    rol_rol: rol_rol.required(),
+    estado_idEstado: estado_idEstado.required(),
+    correo_electronico: correo_electronico.required(),
+    nombre_completo: nombre_completo.required(),
+    password: password.required(),
+    telefono: telefono.required(),
+    fecha_nacimiento: fecha_nacimiento.required()
+});
+
+const updateUsuarioSchema = Joi.object({
+    idUsuario: idUsuario.required(),
+    rol_rol: rol_rol.required(),
+    estado_idEstado: estado_idEstado.required(),
+    correo_electronico: correo_electronico.required(),
+    nombre_completo: nombre_completo.required(),
+    password: password.required(),
+    telefono: telefono.required(),
+    fecha_nacimiento: fecha_nacimiento.required()
+})
+
+module.exports = {createUsuarioSchema, updateUsuarioSchema};
